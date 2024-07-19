e7addb6d5bd95ac9ab53a76e1225668bf3974073
HenryH054 <henryphutchinson@gmail.com>
2024-07-19 11:46:20 -0600

    fancy stuff

--- js/main.js
+++ js/main.js
@@ -14,3 +14,30 @@ function showSection(sectionId) {
 		}
 	});
 }
+
+document.getElementById("themeChange").addEventListener("click", function() {
+
+	toggleStylesheet("./css/theme.css");
+})
+
+function toggleStylesheet(href) {
+	var existingNode = null;
+
+	// Check if stylesheet already exists
+	if (document.styleSheets.length > 1) {
+		existingNode = document.styleSheets[1].ownerNode;
+	}
+
+	if (existingNode) { // If stylesheet exists, remove it
+		console.log(existingNode)
+		existingNode.parentNode.removeChild(existingNode);
+	} else { // If stylesheet does not exist, add it
+		console.log("adding node")
+		var link = document.createElement('link');
+		link.rel = 'stylesheet';
+		link.type = 'text/css';
+		link.href = href;
+		document.getElementsByTagName('head')[0].appendChild(link);
+	}
+}
+
